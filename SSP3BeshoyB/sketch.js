f137bfbd1c6aeb40bc7a60f91b5e474b1c839161
gaming <118367517+Beshoy-Bebawe@users.noreply.github.com>
2026-02-20 13:41:45 -0500

    this is Ball falling of a building
    The Ball when it falls off the building a random line can stop the ball

--- SSP3BeshoyB/sketch.js
+++ SSP3BeshoyB/sketch.js
@@ -1,7 +1,41 @@
+let IsPressed = false;
+let BallY = 30;
+let randomNumber;
 function setup() {
   createCanvas(400, 400);
+   randomNumber = 400;
 }
 
 function draw() {
   background(220);
+  rect(-100,50,200,500);
+  line(400,randomNumber,100,randomNumber);
+
+  circle(200,BallY,40);
+   if(randomNumber>BallY + 20)
+   {
+     BallY += 5;
+   }
+   else if(randomNumber<BallY-20)
+   {
+      BallY += 5;
+   }
+  else if(BallY > 400)
+   {
+      BallY = 30;
+   }
+   else
+   {
+      BallY = 30;
+
+   }
+  
+
+}
+function mousePressed(){
+     
+     
+        randomNumber = random(50,400);
+      IsPressed = true;
+
 }
